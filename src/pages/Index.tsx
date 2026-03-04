b5a3f165c6626f2755d4aaa2b4b8fbde1028a952
Aanand <Aanandmandal0011@gmail.com>
2026-03-04 15:01:39 +0545

    Second

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -326,12 +326,12 @@ const AboutSection = () => {
   {
     name: "Aanand Kumar Mandal",
     role: "Lead Developer, UI/UX Designer, Backend Developer",
-    image: "/public/image/aanand.jpg"
+    image: "/public/images/aanand.jpg"
   },
   {
     name: "Reejon Baruwal",
     role: "Frontend Developer",
-    image: "/public/image/reejon.jpeg"
+    image: "/public/images/reejon.jpeg"
   },
 ]
           .map((dev, i) => (
